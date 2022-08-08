2bec9a65a28eff08e3c38677c7035bfcc94a28a8
oguzhan2912 <oguzhan.yanik@ogr.sakarya.edu.tr>
2022-08-08 17:55:17 +0300

    8.08.2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import  {RegisterpageComponent} from "./page/registerpage/registerpage.component";
+import {SingingComponent} from "./page/singing/singing.component";
+import {CvpageComponent} from "./page/cvpage/cvpage.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "register", pathMatch: "full" },
+  { path: "register", component: RegisterpageComponent },
+  { path: "singing", component: SingingComponent },
+  { path: "cvpage", component: CvpageComponent},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      useHash: true
+    })
+
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+import {PagesModule} from "./page/pages.module";
+import { DegreeModalComponent } from './page/modals/degree-modal/degree-modal.component';
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PagesModule,
+    MatDialogModule,
+    MatButtonModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
