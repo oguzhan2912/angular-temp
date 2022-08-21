c3b42bd95b07d2c783c63e9bb0e24ecb7bf94eda
oguzhan2912 <darkblood.1523@gmail.com>
2022-08-21 15:04:29 +0300

    21.08.2022

---
+++ src/app/Services/LanguageInfo/language-item/language-item.model.ts
@@ -0,0 +1,14 @@
+
+export class LanguageItemModel  {
+
+  languageID:number;
+  languageItemID:number;
+  language:string[];
+  speakingLVL:number[];
+  readingLVL:number[];
+  writingLVL:number[];
+  understandingLVL:number[];
+  languageExam:string[];
+  examResultNumber:number;
+
+}
