cd058a45c7ae7e2555d008e613f1ca24e49afd68
Oğuzhan Yanık <oyanik@turkiyemaarif.org>
2022-12-14 23:53:01 +0300

    User Register

--- src/app/page/registerpage/registerpage.component.ts
+++ src/app/page/registerpage/registerpage.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { RegisterInfoService } from 'src/app/Services/RegisterInfo/register-info.service';
 
 
 
@@ -10,13 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterpageComponent implements OnInit {
 
+  registerForm:FormGroup;
+  constructor(
+  private languagesService:RegisterInfoService) { }
 
-  constructor() { }
 
-
-
+  createNewForm(){
+    this.registerForm=new FormGroup(
+      {
+        userName:new FormControl("",[Validators.required,]),
+        password:new FormControl("",[Validators.required,]),
+      
+      })
+  }
 
   ngOnInit(): void {
+    this.createNewForm();
+  }
+
+  register(){
+    this.languagesService.add(this.registerForm.value).subscribe({
+      next:(res)=>this.languagesService.getList(),
+      error:(err)=>console.log(err)
+    })
+    console.warn(this.registerForm.value);
+    this.createNewForm();
+    
   }
 
 }
